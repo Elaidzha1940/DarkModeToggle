19f90ab30de1ed3f9245fffa5526929b38b8f94e
Elaidzha1940 <eli080293@gmail.com>
2023-10-03 18:22:00 +0300

    DarkModeToggle

---
+++ DarkModeToggle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DarkModeToggle
+//
+//  Created by Elaidzha Shchukin on 03.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DarkModeToggle/DarkModeToggleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeToggleApp.swift
+//  DarkModeToggle
+//
+//  Created by Elaidzha Shchukin on 03.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeToggleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
