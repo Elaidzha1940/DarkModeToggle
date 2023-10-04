317e8729380bfe7194538d2faa849baecbaae9f0
Elaidzha1940 <eli080293@gmail.com>
2023-10-04 18:03:27 +0300

    fixed Colors

--- DarkModeToggle/ContentView.swift
+++ DarkModeToggle/ContentView.swift
@@ -14,48 +14,52 @@ struct ContentView: View {
     
     var body: some View {
         
-        VStack {
-          
+        ZStack {
+            Color(isDarkMode ? Color("BD") : Color("L"))
+                .edgesIgnoringSafeArea(.all)
+            
             VStack {
                 
-            }
-            .padding(.horizontal, 25)
-            .padding(.vertical, 15)
-            
-            Circle()
-                .foregroundStyle(
-                isDarkMode
-                ? Color.white
-                : Color(Color("OR"))
-                )
-                .frame(width: 140, height: 140)
-                .overlay(alignment: .topTrailing) {
-                    if !isDarkMode{
-//                        Circle()
-//                            .frame(width: 20, height: 20)
-//                            .offset(x: 10, y: 15)
-                    } else {
-                        Circle()
-                            .foregroundStyle(Color("Dark"))
-                            .frame(width: 105, height: 105)
-                            .offset(x: 10, y: 15)
-                    }
+                VStack {
+                    
                 }
-            
-            
-            Toggle("", isOn: $isDarkMode)
-                .toggleStyle(CustomToggleStyle())
-                .padding()
+                .padding(.horizontal, 25)
+                .padding(.vertical, 15)
+                
+                Circle()
+                    .foregroundStyle(
+                        isDarkMode
+                        ? Color.white
+                        : Color(Color("OR"))
+                    )
+                    .frame(width: 140, height: 140)
+                    .overlay(alignment: .topTrailing) {
+                        if !isDarkMode{
+                            //                        Circle()
+                            //                            .frame(width: 20, height: 20)
+                            //                            .offset(x: 10, y: 15)
+                        } else {
+                            Circle()
+                                .foregroundStyle(Color("BD"))
+                                .frame(width: 105, height: 105)
+                                .offset(x: 10, y: 5)
+                        }
+                    }
+                
+                
+                Toggle("", isOn: $isDarkMode)
+                    .toggleStyle(CustomToggleStyle())
+                    .padding()
+            }
+            .background(isDarkMode ? Color("BD") : Color("L"))
+            .cornerRadius(20)
+            .frame(width: 290, height: 250)
+            .shadow(
+                color: .black.opacity(0.4),
+                radius: 10,
+                x: 10,
+                y: 10)
         }
-        .preferredColorScheme(.dark)
-        .background(isDarkMode ? Color("Dark") : Color("Light"))
-        .cornerRadius(20)
-        .frame(width: 290, height: 250)
-        .shadow(
-            color: .black.opacity(0.2),
-            radius: 10,
-            x: 10,
-            y: 10)
     }
 }
 
@@ -70,11 +74,12 @@ struct CustomToggleStyle: ToggleStyle {
                 .frame(width: 200, height: 40)
             
             RoundedRectangle(cornerRadius: 20, style: .continuous)
-                .foregroundColor(configuration.isOn ? Color("BD") : Color("Light"))
+                .foregroundColor(configuration.isOn ? Color("BD") : Color("L"))
+            //.foregroundColor(.white)
                 .frame(width: 100, height: 42)
                 .offset(x: configuration.isOn ? 50 : -50)
                 .shadow(
-                    color: .black.opacity(0.4),
+                    color: .white.opacity(0.1),
                     radius: 10,
                     x: 10,
                     y: 10)
@@ -86,7 +91,7 @@ struct CustomToggleStyle: ToggleStyle {
                                 weight: .bold,
                                 design: .rounded))
                     .foregroundStyle(configuration.isOn ? .gray : .black)
-
+                
                 Spacer()
                 
                 Text("Dark")
