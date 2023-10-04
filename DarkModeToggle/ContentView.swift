a3f1d074c86102261dcba0a744ee9220e2204964
Elaidzha1940 <eli080293@gmail.com>
2023-10-04 15:09:18 +0300

    Updated CustomToggleStyle

--- DarkModeToggle/ContentView.swift
+++ DarkModeToggle/ContentView.swift
@@ -1,24 +1,50 @@
+//  /*
 //
-//  ContentView.swift
-//  DarkModeToggle
+//  Project: DarkModeToggle
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 03.10.2023
 //
-//  Created by Elaidzha Shchukin on 03.10.2023.
-//
+//  */
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var isDarkMode: Bool = false
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+          
+            VStack {
+                
+            }
+            .padding(.horizontal, 25)
+            .padding(.vertical, 15)
+            Circle()
+            Toggle("", isOn: $isDarkMode)
+                .toggleStyle(CustomToggleStyle())
+                .padding()
         }
-        .padding()
+        .background(.white)
+        .frame(width: 290, height: 250)
+        .cornerRadius(20)
+        .shadow(
+            color: .black.opacity(0.5),
+            radius: 10,
+            x: 10,
+            y: 10)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct CustomToggleStyle: ToggleStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            
+        }
+    }
 }
--- DarkModeToggle/DarkModeToggleApp.swift
+++ DarkModeToggle/DarkModeToggleApp.swift
@@ -1,9 +1,11 @@
+//  /*
 //
-//  DarkModeToggleApp.swift
-//  DarkModeToggle
+//  Project: DarkModeToggle
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 03.10.2023
 //
-//  Created by Elaidzha Shchukin on 03.10.2023.
-//
+//  */
 
 import SwiftUI
 
